506120d33a634fd4fb43734404580b92a81dc699
Drew Ruppel <drewruppel@gmail.com>
2019-09-30 16:13:18 -0700

    initial commit

---
+++ bot.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const fs = require('fs');
+const discord = require('discord.js');
+const client = new discord.Client();
+
+const config = JSON.parse(
+    fs.readFileSync(
+        'config/config.json',
+        'utf8'
+    )
+);
+
+client.on('ready', () => {
+    console.log('Bot ready');
+});
+
+client.on('message',  (message) => {
+    if (message.content.startsWith('ping')) {
+        message.channel.send('pong');
+    }
+});
+
+client.login(config.token);
