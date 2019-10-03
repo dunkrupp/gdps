b784c1db34fcd5597109ffae22dd1624c8c70172
Drew Ruppel <drewruppel@gmail.com>
2019-10-03 16:00:16 -0700

    Add Main Command Loop

--- index.js
+++ index.js
@@ -1,20 +1,62 @@
 'use strict'
 
-require('dotenv').config()
+/** Config */
+const config = require('./config/app.json')
+const pfx = config.prefix
 
+/** Autoload */
+const app = require('./app/autoload')
 const discord = require('discord.js')
-const offender = require('./lib/offender')
-const citation = require('./lib/citation')
+
+/** Classes */
 const client = new discord.Client()
+const help = new app.Help()
 
 client.on('ready', () => {
-  console.log('Bot ready')
+  const fs = require('fs')
+
+  fs.access(config.database, error => {
+    if (!error) {
+      console.log('Database Ready')
+    } else {
+      console.log('No Database Found')
+    }
+  })
+  console.log('Bot Ready')
 })
 
-client.on('message', (message) => {
-  if (message.author.bot) {
+client.on('message', message => {
+  if (
+    message.author.bot ||
+    !message.content.startsWith(pfx)
+  ) {
     return false
+  }
+
+  if (message.content.startsWith(pfx)) {
+    const command = message.content.trimStart()
+
+    console.log(command)
+
+    switch (command) {
+      case pfx + 'help':
+        console.log(help)
+        message.channel.send(help.message)
+        break
+      case pfx + 'roe':
+        message.channel.send('roe test')
+        break
+      case pfx + 'citations':
+        message.channel.send('citations test')
+        break
+      case pfx + 'player':
+        message.channel.send('player test')
+        break
+      case pfx + 'config':
+        message.channel.send('config test')
+        break
+    }
   }
 })
 
-client.login(process.env.BOT_TOKEN)
+client.login(config.token)
